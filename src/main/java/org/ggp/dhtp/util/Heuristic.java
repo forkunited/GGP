adc0bac13fc21ccbb60090fe9f2d6fc98ad270a6
Bill McDowell <forkunited@gmail.com>
2017-04-23 15:49:15 -0700

    Added mobility heuristic and updated the ggp codebase with course changes

--- src/main/java/org/ggp/dhtp/util/Heuristic.java
+++ src/main/java/org/ggp/dhtp/util/Heuristic.java
@@ -1,9 +1,12 @@
 package org.ggp.dhtp.util;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public abstract class Heuristic {
-	public abstract int evalState(Role role, MachineState state);
+	public abstract int evalState(Role role, MachineState state) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException;
 }
 
 
