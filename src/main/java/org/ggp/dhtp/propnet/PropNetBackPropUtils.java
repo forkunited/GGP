3b0b1c9b09431f595aea1002c202a47ebf7d7f6c
Wyatt <wdaviau@stanford.edu>
2017-05-13 19:16:35 -0700

    Adding propNetUtils back prop class

---
+++ src/main/java/org/ggp/dhtp/propnet/PropNetBackPropUtils.java
@@ -0,0 +1,28 @@
+package org.ggp.dhtp.propnet;
+
+
+import java.util.List;
+
+import org.ggp.base.util.propnet.architecture.PropNet;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+
+public class PropNetBackPropUtils {
+	/* Update the assignment of a propnet given the new state */
+	public static void markBases(MachineState state, PropNet propnet) {
+		return;
+	}
+
+	/* */
+	public static void markActions(MachineState state, List<Move> move, PropNet propnet) {
+		return;
+	}
+
+	/* Read the assignment of a proposition */
+
+	public static boolean propMarkP() {
+		return true;
+	}
+
+
+}
